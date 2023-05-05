e8c0801768b49046458c441fd8c080869ffc96c6
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2023-05-05 11:24:39 +0530

    Create N_dimensional_array.py

---
+++ N_dimensional_array.py
@@ -0,0 +1,32 @@
+#importing numpy
+import numpy as np
+
+array1 =( [
+    [2,3,5],
+    [4,6,8]
+    ])
+#implementing some functions on numpy 
+print("Type of array is: ",type(array1))
+
+print("Length of array: ",len(array1))
+
+print(array1.ndim)
+print(array1.size)
+
+print(array1.shape)
+print("Array store element of array: ",array1.dtype())
+
+
+"""
+import numpy as np
+
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
+"""
