24461a0af4d05a66ff991b5df0cbd748701cb52b
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2023-07-21 13:33:34 +0530

    Add files via upload

---
+++ aggregation_rel.py
@@ -0,0 +1,28 @@
+class Customer:
+    def __init__(self,name,gender,address):
+        self.name = name
+        self.gender = gender
+        self.address = address
+
+    def edit_profile(self,new_name,new_city,new_pin,new_state):
+        self.name=new_name
+        self.address.change_address(new_city,new_pin,new_state)
+
+
+class Address:
+    def __init__(self,city,pincode,state):
+        self.city=city
+        self.pincode = pincode
+        self.state=state
+
+    def change_address(self,new_city,new_pin,new_state):
+        self.city=new_city
+        self.pincode = new_pin
+        self.state = new_state
+        
+
+add = Address('Kolkata','492101',"MP")
+cust = Customer("Pk",'Male',add)
+
+cust.edit_profile("Ravi","Goa",12445,"Maharastra")
+print(cust.address.pincode)
