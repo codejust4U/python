f5da727f015247f981baa25efa6e90f1bb38b7bd
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2023-08-24 08:20:03 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,62 +1,65 @@
-import requests
 import streamlit as st
 import pickle
-import pandas as pd
+import numpy as np
 
-def fetch_poster(movie_id):
-    response=requests.get('https://api.themoviedb.org/3/movie/{}?api_key=1271d0f04e77da71552748bb8135c633&language=en-US'.format(movie_id))
-    data = response.json()
-    #st.text(data)
-    #st.text('https://api.themoviedb.org/3/movie/{}?api_key=1271d0f04e77da71552748bb8135c633&language=en-US'.format(movie_id))
-    return  "https://image.tmdb.org/t/p/w500/"+data['poster_path']
+# import the model
+pipe = pickle.load(open('pipe.pkl','rb'))
+df = pickle.load(open('df.pkl','rb'))
 
-def recommend(movie):
-    movie_index=movies[movies['title']==movie].index[0]
-    distances = similarity[movie_index]
-    movie_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+st.title("Laptop Predictor")
 
-    recommended_movies= []
-    recommended_movies_poster = []
+# brand
+company = st.selectbox('Brand',df['Company'].unique())
 
-    for i in movie_list:
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movies.append(movies.iloc[i[0]].title)
-        #fetch poster from API
-        recommended_movies_poster.append(fetch_poster(movie_id))
+# type of laptop
+type = st.selectbox('Type',df['TypeName'].unique())
 
-    return recommended_movies, recommended_movies_poster
+# Ram
+ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
 
+# weight
+weight = st.number_input('Weight of the Laptop')
 
-movies_dict = pickle.load(open('movie_dict.pkl','rb'))
-movies = pd.DataFrame(movies_dict)
+# Touchscreen
+touchscreen = st.selectbox('Touchscreen',['No','Yes'])
 
-similarity = pickle.load(open('similarity.pkl','rb'))
+# IPS
+ips = st.selectbox('IPS',['No','Yes'])
 
-st.title('Movie Recommender System')
+# screen size
+screen_size = st.number_input('Screen Size')
 
-selected_movie_name = st.selectbox(
-    'About which movie you want to know?',movies['title'].values
-)
+# resolution
+resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
 
-if st.button('Recommend'):
-    names, posters=recommend(selected_movie_name)
-    col1,col2,col3,col4,col5 = st.columns(5)
-    with col1:
-        st.text(names[0])
-        st.image(posters[0])
-    with col2:
-        st.text(names[1])
-        st.image(posters[1])
-    with col3:
-        st.text(names[2])
-        st.image(posters[2])
-    with col4:
-        st.text(names[3])
-        st.image(posters[3])
-    with col5:
-        st.text(names[4])
-        st.image(posters[4])
+#cpu
+cpu = st.selectbox('CPU',df['Cpu brand'].unique())
 
+hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
 
+ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
 
+gpu = st.selectbox('GPU',df['Gpu brand'].unique())
 
+os = st.selectbox('OS',df['os'].unique())
+
+if st.button('Predict Price'):
+    # query
+    ppi = None
+    if touchscreen == 'Yes':
+        touchscreen = 1
+    else:
+        touchscreen = 0
+
+    if ips == 'Yes':
+        ips = 1
+    else:
+        ips = 0
+
+    X_res = int(resolution.split('x')[0])
+    Y_res = int(resolution.split('x')[1])
+    ppi = ((X_res**2) + (Y_res**2))**0.5/screen_size
+    query = np.array([company,type,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
+
+    query = query.reshape(1,12)
+    st.title("The predicted price of this configuration is " + str(int(np.exp(pipe.predict(query)[0]))))
