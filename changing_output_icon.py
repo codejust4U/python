438b24ef757032dd29c8c9cb936fac7da1c3479a
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2023-12-30 11:53:33 +0530

    Add files via upload

---
+++ changing_output_icon.py
@@ -0,0 +1,10 @@
+# changing output icon
+
+# changing the output icon
+import tkinter as tk
+
+root = tk.Tk()
+
+root.wm_iconbitmap(r"C:\Users\panka\OneDrive\Pictures\Screenshots\btmp.png")
+
+root.mainloop()
