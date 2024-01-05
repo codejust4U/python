d17f2253de2f52478780eb82a5edf443e5285a85
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2024-01-05 14:48:13 +0530

    Add files via upload

---
+++ messagebox5.py
@@ -0,0 +1,13 @@
+#importing the modules
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.geometry("300x200") 
+  
+w = Label(root, text ='GeeksForGeeks', font = "50")  
+w.pack() 
+
+messagebox.askokcancel('Ok?','want to continue?')
+
+root.mainloop()
